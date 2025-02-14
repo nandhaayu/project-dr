c62b1d504e15c6c7aa0f9859da39d6767ba40d38
nandha <nandha.nandha131102@gmail.com>
2025-02-14 15:31:29 +0700

    bug fixes

--- resources/views/backend/galeri/index.blade.php
+++ resources/views/backend/galeri/index.blade.php
@@ -4,8 +4,10 @@
   <div class="card">
     <div class="card-body">
       <h5 class="card-title fw-semibold mb-4">Galeri</h5>
-          <a href="{{ route('galeri.create') }}" class="bg-green-600 mb-2 px-2 py-2 rounded-lg text-white hover:bg-green-700">+ Tambah Data</a>
-        <div class="table-responsive">
+      <a href="{{ route('galeri.create') }}" class="bg-green-600 mb-2 px-2 py-2 rounded-lg text-white hover:bg-green-700">
+        + Tambah Data
+      </a>
+      <div class="table-responsive">
         <table class="table table-bordered mt-4">
           <thead>
             <tr>
@@ -21,18 +23,26 @@
               <tr>
                 <td>{{ $loop->iteration }}</td>
                 <td>{{ $d->judul }}</td>
-                <td><img src="{{ asset('assets/images/' . $d->foto) }}" alt="project-image" class="rounded" style="width: 100%; max-width: 100px; height: auto;"></td>
                 <td>
-                    <a href="{{ route('galeri.edit', $d->id) }}" class="btn btn-warning btn-sm">Edit</a>
-                    <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal{{ $d->id }}">Hapus</button>
-                    @include('backend.galeri.delete')
+                  @php
+                    $imagePath = asset('assets/images/' . $d->foto);
+                    if (!file_exists(public_path('assets/images/' . $d->foto))) {
+                        $imagePath = asset('assets/images/nophoto.jpg'); // Gambar default jika tidak ditemukan
+                    }
+                  @endphp
+                  <img src="{{ $imagePath }}" alt="Galeri Image" class="rounded" style="width: 100px; height: auto;">
                 </td>
+                <td>
+                  <a href="{{ route('galeri.edit', $d->id) }}" class="btn btn-warning btn-sm">Edit</a>
+                  <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal{{ $d->id }}">Hapus</button>
+                  @include('backend.galeri.delete')
+                </td>
+              </tr>
               @endforeach
-          </tr>
             @else
-                <tr>
-                    <td colspan="4" class="text-center">Data tidak ditemukan</td>
-                </tr>
+              <tr>
+                <td colspan="4" class="text-center">Data tidak ditemukan</td>
+              </tr>
             @endif
           </tbody>
         </table>
