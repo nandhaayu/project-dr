89030e726bf1bc9289485068a7c1e55d7efabd98
nandha <nandha.nandha131102@gmail.com>
2025-02-14 16:46:29 +0700

    [bug fixes]

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(ProfilSeeder::class);
-        $this->call(KurikulumSeeder::class);
         User::factory()->create([
             'name' => 'admin',
             'username' => 'admin',
