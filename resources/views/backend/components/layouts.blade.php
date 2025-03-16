eca94c97885f60869e87adc99d128054efb54198
nandha <nandha.nandha131102@gmail.com>
2025-03-16 15:01:36 +0700

    [fix] : bug fixes

--- resources/views/backend/components/layouts.blade.php
+++ resources/views/backend/components/layouts.blade.php
@@ -13,7 +13,7 @@
   <link rel="stylesheet" href="{{ asset('backend/summernote/summernote.min.css') }}">
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
 
-  <title>PONPES DAARUR RAHMAH</title>
+  <title>PONPES DARUR RAHMAH</title>
 </head>
 
 <body class="h-full flex">
