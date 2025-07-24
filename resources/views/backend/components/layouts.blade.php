01c908e70ff96d63680e3d92c0ba5e6c47359f7c
nandha <nandha.nandha131102@gmail.com>
2025-07-25 00:44:10 +0700

    [bug fixes]

--- resources/views/backend/components/layouts.blade.php
+++ resources/views/backend/components/layouts.blade.php
@@ -4,10 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="preload" href="https://rsms.me/inter/inter.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
-  <noscript><link rel="stylesheet" href="https://rsms.me/inter/inter.css"></noscript>
   <link rel="icon" href="{{ asset('assets/img/logo-PPDR.png') }}" type="image/png" />
-  <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
   <link rel="stylesheet" href="{{ asset('backend/css/styles.min.css') }}" />
   <link rel="stylesheet" href="{{ asset('backend/summernote/summernote.min.css') }}">
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
@@ -29,6 +26,7 @@
     </main>
   </div>
 </body>
+<script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 <script defer  src="{{ asset('backend/libs/jquery/dist/jquery.min.js') }}"></script>
 <script defer  src="{{ asset('backend/libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
 <script defer  src="{{ asset('backend/js/sidebarmenu.js') }}"></script>
