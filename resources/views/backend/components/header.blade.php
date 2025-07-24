f8c0f9d6b69d0c47672a0a06d3464d532db38a21
nandha <nandha.nandha131102@gmail.com>
2025-07-24 23:08:51 +0700

    [bug fixes]

--- resources/views/backend/components/header.blade.php
+++ resources/views/backend/components/header.blade.php
@@ -9,13 +9,9 @@
     
     <!-- Notifikasi -->
     <div class="relative mr-3">
-      <a href="javascript:void(0)" id="notifToggle" class="nav-link nav-icon-hover relative">
-        <!-- Ikon Lonceng -->
-        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-gray-700 hover:text-black" fill="none"
-             viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-          <path stroke-linecap="round" stroke-linejoin="round"
-                d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6 6 0 00-5-5.917V4a1 1 0 00-2 0v1.083A6 6 0 006 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-        </svg>
+      <a href="javascript:void(0)" id="notifToggle" class="nav-link nav-icon-hover relative text-gray-700 hover:text-black">
+        <!-- Ikon Lonceng Font Awesome -->
+        <i class="fas fa-bell text-xl"></i>
 
         @if ($jumlahNotifikasi > 0)
           <span class="absolute -top-1 -right-2 bg-red-600 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full">
